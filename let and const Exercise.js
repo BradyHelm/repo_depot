4a3eed8c177bc72a771a8a1c6e1a19495aa5cbbf
BradyHelm <120422155+BradyHelm@users.noreply.github.com>
2023-01-12 19:34:06 -0700

    Add files via upload
    Answers to let and const exercise.

---
+++ let and const Exercise.js
@@ -0,0 +1,13 @@
+
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+// solution:
+const PI = 3.14;
+
+
+
+// Quiz
+// What is the difference between var and let? var is function scope and let is block scope
+// What is the difference between var and const? var is function scope and block is block scope const can't be  reassigned or redeclared.
+// What is the difference between let and const? you can  reassigned let you can't  reassign const.
+// What is hoisting? when a variable is used before it's declaired.
